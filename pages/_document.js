91375e1f39ea134c1ca7927fba02c067e1794137
Patrick Krawczykowski <pkrawz@gmail.com>
2018-09-23 12:42:21 -0500

    adds data to homepage

--- pages/_document.js
+++ pages/_document.js
@@ -1,16 +1,19 @@
-import Document, {Head, Main, NextScript} from "next/document"
-import {ServerStyleSheet} from 'styled-components'
+import Document, { Head, Main, NextScript } from "next/document"
+import { ServerStyleSheet } from "styled-components"
 
-const getFacebookCrap = () => new Promise((resolve, reject) => {
-  setTimeout(() => {
-    return resolve("This is a cool facebook thing")
-  }, 1000)
-})
+const getFacebookCrap = () =>
+  new Promise((resolve, reject) => {
+    setTimeout(() => {
+      return resolve("This is a cool facebook thing")
+    }, 1000)
+  })
 
 export default class MyDocument extends Document {
   static async getInitialProps(ctx) {
     const sheet = new ServerStyleSheet()
-    const page = ctx.renderPage(App => props => sheet.collectStyles(<App {...props} />))
+    const page = ctx.renderPage(App => props =>
+      sheet.collectStyles(<App {...props} />)
+    )
     const styleTags = sheet.getStyleElement()
     const result = await getFacebookCrap()
     return {
@@ -21,15 +24,17 @@ export default class MyDocument extends Document {
   }
 
   render() {
-    return (<html>
-      <Head>
-        {this.props.styleTags}
-        <meta name="og:title" content={this.props.result}/>
-      </Head>
-      <body className="custom_class">
-        <Main/>
-        <NextScript/>
-      </body>
-    </html>)
+    return (
+      <html>
+        <Head>
+          {this.props.styleTags}
+          <meta name="og:title" content={this.props.result} />
+        </Head>
+        <body className="custom_class">
+          <Main />
+          <NextScript />
+        </body>
+      </html>
+    )
   }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,26 +1,39 @@
 import Hero from "../components/Hero"
 import { colors } from "../constants"
-import Card from "../components/Card"
+import { createClient } from "contentful"
 
-export default props => (
-  <main>
-    <Hero />
-    <Card />
+export default class extends React.Component {
+  static async getInitialProps() {
+    const client = createClient({
+      space: "021ulla0m5co",
+      accessToken:
+        "709d8f98e56158641d25ba23ceb57029ecc91e0a9a11f35fa6e0f666ffbeb4d0",
+      host: "preview.contentful.com"
+    })
+    const data = await client.getEntries()
+    return { data }
+  }
+  render() {
+    return (
+      <main>
+        <Hero />
 
-    <style global jsx>{`
-      * {
-        box-sizing: border-box;
-      }
-      :root {
-        font-size: 75%;
-      }
-      body {
-        margin: 0;
-        padding: 0;
-        font-size: 1.5rem;
-        font-family: "IBM Plex Mono", monospace;
-        background: ${colors.grey_100};
-      }
-    `}</style>
-  </main>
-)
+        <style global jsx>{`
+          * {
+            box-sizing: border-box;
+          }
+          :root {
+            font-size: 75%;
+          }
+          body {
+            margin: 0;
+            padding: 0;
+            font-size: 1.5rem;
+            font-family: "IBM Plex Mono", monospace;
+            background: ${colors.grey_100};
+          }
+        `}</style>
+      </main>
+    )
+  }
+}
