79ffd63562c7f6b9277f8604575afe1719c4c005
Patrick Krawczykowski <pkrawz@gmail.com>
2018-09-30 13:18:28 -0500

    styles the lessons index page

--- pages/lessons/index.js
+++ pages/lessons/index.js
@@ -1,22 +1,24 @@
 import styled from "styled-components"
 import client from "contentfulClient"
 import Link from "next/link"
+import { colors } from "constants"
 
 const Lessons = props => (
-  <LessonsWrapper className="lesson-list container">
+  <LessonsWrapper className="container">
+    <h1>Helpful Tidbits</h1>
     {props.lessons.map(lesson => (
-      <div className="lesson-list__item">
+      <LessonItem lessonBg={lesson.fields.poster.fields.file.url}>
         <div className="copy">
           <h3>{lesson.fields.title}</h3>
           <Link
-            as={`/l/${lesson.sys.id}`}
             href={`/lessons/show?id=${lesson.sys.id}`}
+            as={`/l/${lesson.sys.id}`}
           >
             <a>Read more.</a>
           </Link>
         </div>
-        <img src={lesson.fields.poster.fields.file.url} alt="" />
-      </div>
+        <div className="lesson-image" />
+      </LessonItem>
     ))}
   </LessonsWrapper>
 )
@@ -31,15 +33,42 @@ Lessons.getInitialProps = async () => {
 
 const LessonsWrapper = styled.section`
   position: relative;
-  margin-top: 6rem;
-  .lesson-list__item {
+  max-width: 75rem;
+  margin: 10rem auto 0;
+`
+
+const LessonItem = styled.article`
+  display: flex;
+  margin-top: 5rem;
+  height: 20rem;
+  position: relative;
+  background: ${colors.orange_100};
+  .copy {
+    position: relative;
+    padding: 2rem;
     display: flex;
-    justify-content: space-around;
-    margin-top: 2rem;
+    flex-direction: column;
+    width: 75%;
+    z-index: 1;
+    a {
+      margin-top: auto;
+    }
   }
-  img {
-    height: 20rem;
-    margin-left: auto;
+  .lesson-image {
+    position: absolute;
+    height: 100%;
+    right: 0;
+    width: 50%;
+    background: linear-gradient(
+        280deg,
+        rgba(255, 217, 205, 0) 0%,
+        rgba(255, 217, 205, 0.6) 48%,
+        rgba(255, 217, 205, 1) 88%
+      ),
+      url(${({ lessonBg }) => lessonBg});
+    background-repeat: no-repeat;
+    background-size: cover;
+    z-index: 0;
   }
 `
 
