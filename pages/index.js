80aed33e52b9cc9c1c8a1fe5ab405b1a0a06babd
Patrick Krawczykowski <pkrawz@gmail.com>
2019-03-04 19:25:21 -0600

    Add homepage data

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,68 @@
 import styled from "styled-components"
-import { colors } from "constants"
+import { colors, apiUrl } from "constants"
+import Prismic from "prismic-javascript"
+import { RichText } from "prismic-reactjs"
+import Link from "next/link"
+import Banner from "components/banner"
+import FeaturedLessons from "components/featured-lessons"
+import Courses from "components/courses"
+import Footer from "components/footer"
 
-export default props => {
+const Homepage = ({
+  document: {
+    data: {
+      hero_title,
+      featured_lessons,
+      courses,
+      body: [banner]
+    }
+  }
+}) => {
   return (
     <Main>
-      <h1>React Boilerplate</h1>
+      <section className="hero">
+        <div className="container">
+          <h1>{RichText.render(hero_title)}</h1>
+          <Link href="/about">
+            <a className="block-link">Learn more about us.</a>
+          </Link>
+        </div>
+      </section>
+      {banner && (
+        <div className="container">
+          <Banner data={banner.primary} />
+        </div>
+      )}
+      <FeaturedLessons featuredLessons={featured_lessons} />
+      <Courses data={courses} />
+      <Footer />
     </Main>
   )
 }
 
+Homepage.getInitialProps = async () => {
+  try {
+    const api = await Prismic.api(apiUrl)
+    const document = await api.getSingle("home")
+    return { document }
+  } catch (error) {
+    return { error }
+  }
+}
+
 const Main = styled.main`
   min-height: 100vh;
-  text-align: center;
-  h1 {
-    padding: 5rem 3rem 3rem;
-    background: ${colors.black};
+  .hero {
+    padding: 9rem 0 15rem;
+    background: ${colors.grey_900};
+    color: ${colors.grey_100};
+    .container {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-end;
+      min-height: 40vh;
+    }
   }
 `
+
+export default Homepage
