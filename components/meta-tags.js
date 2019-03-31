65a8429db7f0c440ecf7b80fac733f6e3c3a2514
Patrick Krawczykowski <patrick@dreadful.design>
2019-03-31 14:38:38 -0500

    add default props for MetaTag

--- components/meta-tags.js
+++ components/meta-tags.js
@@ -1,12 +1,18 @@
 import React from "react"
 import Head from "next/head"
 
-export default function MetaTags({ title, description, image }) {
+export default function MetaTags({
+  title = "State Matters | Track your local government",
+  description,
+  image,
+  type = "website"
+}) {
   return (
     <Head>
       <meta property="og:title" content={title} />
       <meta property="og:description" content={description} />
       <meta property="og:image" content={image} />
+      <meta property="og:type" content={type} />
       <meta name="twitter:title" content={title} />
       <meta name="twitter:description" content={description} />
       <meta name="twitter:image" content={image} />
