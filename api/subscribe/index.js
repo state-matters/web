37170ea1596e93ecc96db5ece30cd785579007ca
Patrick Krawczykowski <patrick@dreadful.design>
2019-04-01 21:52:23 -0500

    add axios and secret key

--- api/subscribe/index.js
+++ api/subscribe/index.js
@@ -1,14 +1,38 @@
 const express = require("express")
+const axios = require("axios")
 
 const app = express()
 
 app.use(express.json())
 
-app.post("*", (req, res, next) => {
-  if (!req.params.name || !req.params.email) {
-    res.status(422).json({ error: "Need name and email to subscribe" })
+const mailchimpUrl = "https://us17.api.mailchimp.com/3.0/lists/ea066b0443/members"
+
+app.post("*", async (req, res) => {
+  if (!req.body.email || !req.body.name) {
+    res.send(422, { error: "Need name and email to subscribe" })
   }
-  res.status(200).json({ email: req.params.email })
+  console.log(req.body)
+  try {
+    const response = await axios({
+      method: "post",
+      url: mailchimpUrl,
+      auth: {
+        username: "state_matters",
+        password: process.env.MAIL_API_KEY
+      },
+      data: JSON.stringify({
+        email_address: req.body.email,
+        status: "subscribed",
+        merge_fields: {
+          NAME: req.body.name
+        }
+      })
+    })
+    console.log(response)
+    res.status(200).json({ name: req.body.name })
+  } catch (error) {
+    res.status(500).json({ error: error.response.data.detail })
+  }
 })
 
 module.exports = app
