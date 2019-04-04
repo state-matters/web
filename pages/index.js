7c32ef4a181ee2bcb92a90f97c292cf45c94f0ce
Patrick Krawczykowski <patrick@dreadful.design>
2019-04-03 19:46:16 -0500

    update courses to conditionally render

--- pages/index.js
+++ pages/index.js
@@ -37,7 +37,7 @@ const Homepage = ({
         </Container>
       )}
       <FeaturedLessons featuredLessons={featured_lessons} />
-      <Courses courses={courses} />
+      {courses.length && <Courses courses={courses} />}
       <Footer />
     </Main>
   )
